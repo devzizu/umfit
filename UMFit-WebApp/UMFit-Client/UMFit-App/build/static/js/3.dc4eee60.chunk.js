924f2f7e7a84fe0123e0b42f2fe5a4e255d382f8
hchexy <lolturklol@gmail.com>
2020-06-15 21:50:27 +0100

    package

--- UMFit-WebApp/UMFit-Client/UMFit-App/build/static/js/3.dc4eee60.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpUMFit-App"]=this["webpackJsonpUMFit-App"]||[]).push([[3],{385:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(48),o=(e(14),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.l)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=3.dc4eee60.chunk.js.map
