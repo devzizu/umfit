2a148dc504389dbf218f468a1958cb03d625fade
hchexy <lolturklol@gmail.com>
2020-07-02 18:28:23 +0100

    Manifest 4

---
+++ UMFit-WebApp/UMFit-Client/UMFit-App/build/precache-manifest.7482dc9abb654cbfe9bf535570b78385.js
@@ -0,0 +1,558 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e192ba09a0fdad97c268e9e13010c49",
+    "url": "/li4_umfit/index.html"
+  },
+  {
+    "revision": "221ad67d50b08c48eadc",
+    "url": "/li4_umfit/static/css/10.1d96f52d.chunk.css"
+  },
+  {
+    "revision": "e596d07d254e84050f3e",
+    "url": "/li4_umfit/static/css/main.7da560a2.chunk.css"
+  },
+  {
+    "revision": "92bc9b1b3ca77c758c8a",
+    "url": "/li4_umfit/static/js/0.95a0670c.chunk.js"
+  },
+  {
+    "revision": "3661f572a51261f8c7e1",
+    "url": "/li4_umfit/static/js/1.96916fa1.chunk.js"
+  },
+  {
+    "revision": "221ad67d50b08c48eadc",
+    "url": "/li4_umfit/static/js/10.f55f8f49.chunk.js"
+  },
+  {
+    "revision": "989cfaa4bd17dedf4ad8a678add7cf8e",
+    "url": "/li4_umfit/static/js/10.f55f8f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf335d786d70dab0fed",
+    "url": "/li4_umfit/static/js/11.02f3be71.chunk.js"
+  },
+  {
+    "revision": "2e7163643eb19989aab5",
+    "url": "/li4_umfit/static/js/12.ae479627.chunk.js"
+  },
+  {
+    "revision": "479dd994382280aa98c6",
+    "url": "/li4_umfit/static/js/13.c7b86819.chunk.js"
+  },
+  {
+    "revision": "e621d918b8c4812f07f3",
+    "url": "/li4_umfit/static/js/14.08fe8180.chunk.js"
+  },
+  {
+    "revision": "4afd369282ca13fdf82b",
+    "url": "/li4_umfit/static/js/15.e47b6fea.chunk.js"
+  },
+  {
+    "revision": "5630c9f7635ab980b443",
+    "url": "/li4_umfit/static/js/16.0d6f1df0.chunk.js"
+  },
+  {
+    "revision": "5e5468cfbab10076cc91",
+    "url": "/li4_umfit/static/js/17.2067d1a7.chunk.js"
+  },
+  {
+    "revision": "3de77259e878e5c4bf30",
+    "url": "/li4_umfit/static/js/18.da1477ff.chunk.js"
+  },
+  {
+    "revision": "f67fa7b663a3b3b54eff",
+    "url": "/li4_umfit/static/js/19.14e5fb8e.chunk.js"
+  },
+  {
+    "revision": "7f5aaf9060e0ea550778",
+    "url": "/li4_umfit/static/js/2.5c03de6f.chunk.js"
+  },
+  {
+    "revision": "f592a627befd93f83ad7",
+    "url": "/li4_umfit/static/js/20.e413a2f4.chunk.js"
+  },
+  {
+    "revision": "af0e89e275283c43d089",
+    "url": "/li4_umfit/static/js/21.0a6359c6.chunk.js"
+  },
+  {
+    "revision": "0c97e2ea7e7ddd7b6401",
+    "url": "/li4_umfit/static/js/22.aaddc590.chunk.js"
+  },
+  {
+    "revision": "ba589ba739422e7c8625",
+    "url": "/li4_umfit/static/js/23.4700df95.chunk.js"
+  },
+  {
+    "revision": "6a575141e0fae4c7c83f",
+    "url": "/li4_umfit/static/js/24.64ddfd6f.chunk.js"
+  },
+  {
+    "revision": "613638468aae92980e01",
+    "url": "/li4_umfit/static/js/25.841a893b.chunk.js"
+  },
+  {
+    "revision": "8594ca6928bdf8833b65",
+    "url": "/li4_umfit/static/js/26.20f0359e.chunk.js"
+  },
+  {
+    "revision": "fbc4d550295e9bfcde11",
+    "url": "/li4_umfit/static/js/27.40458edf.chunk.js"
+  },
+  {
+    "revision": "23c728de5a039b680d85",
+    "url": "/li4_umfit/static/js/28.147e1817.chunk.js"
+  },
+  {
+    "revision": "84337fd5b7deb62a87b9",
+    "url": "/li4_umfit/static/js/29.3d60e996.chunk.js"
+  },
+  {
+    "revision": "7865d7be26ea2f16f03d",
+    "url": "/li4_umfit/static/js/3.01389bb6.chunk.js"
+  },
+  {
+    "revision": "b03a30d3b403f9c4e4b8",
+    "url": "/li4_umfit/static/js/30.eef94317.chunk.js"
+  },
+  {
+    "revision": "fb0d2dbe997d42493ede",
+    "url": "/li4_umfit/static/js/31.21cba946.chunk.js"
+  },
+  {
+    "revision": "700158f26d33278e85df",
+    "url": "/li4_umfit/static/js/32.b690699f.chunk.js"
+  },
+  {
+    "revision": "bbcdc7fe29aa46e72248",
+    "url": "/li4_umfit/static/js/33.16d7b395.chunk.js"
+  },
+  {
+    "revision": "090f833bdcedc431237c",
+    "url": "/li4_umfit/static/js/34.fafa70da.chunk.js"
+  },
+  {
+    "revision": "e77d54d699157df75e45",
+    "url": "/li4_umfit/static/js/35.def2f7d5.chunk.js"
+  },
+  {
+    "revision": "49182ded42cfb9b0bb83",
+    "url": "/li4_umfit/static/js/36.34f8ff91.chunk.js"
+  },
+  {
+    "revision": "1f3f77552f2667791cef",
+    "url": "/li4_umfit/static/js/37.5cae94fc.chunk.js"
+  },
+  {
+    "revision": "49ec7ddc022fca701786",
+    "url": "/li4_umfit/static/js/38.0dc08637.chunk.js"
+  },
+  {
+    "revision": "bbc92d97b0e0667ba65a",
+    "url": "/li4_umfit/static/js/39.82ab0777.chunk.js"
+  },
+  {
+    "revision": "e232daed274ada001945",
+    "url": "/li4_umfit/static/js/4.59c9c5bd.chunk.js"
+  },
+  {
+    "revision": "6373ecd81b3b12d821de",
+    "url": "/li4_umfit/static/js/40.b79ab560.chunk.js"
+  },
+  {
+    "revision": "3b57d92eda5cfbfc0ca5",
+    "url": "/li4_umfit/static/js/41.eb9cba0e.chunk.js"
+  },
+  {
+    "revision": "9fdca7b251f1095112ac",
+    "url": "/li4_umfit/static/js/42.d1602718.chunk.js"
+  },
+  {
+    "revision": "678f31c43ff4eef282c5",
+    "url": "/li4_umfit/static/js/43.e86fe083.chunk.js"
+  },
+  {
+    "revision": "a0681b1b72fad324cfc6",
+    "url": "/li4_umfit/static/js/44.0c041631.chunk.js"
+  },
+  {
+    "revision": "22dd6d8fd656b064921d",
+    "url": "/li4_umfit/static/js/45.21590dc9.chunk.js"
+  },
+  {
+    "revision": "29ca9e48dc2168b8597d",
+    "url": "/li4_umfit/static/js/46.7264714d.chunk.js"
+  },
+  {
+    "revision": "50408e254213d1666423",
+    "url": "/li4_umfit/static/js/47.c9da2260.chunk.js"
+  },
+  {
+    "revision": "a3ce49e77d2d2b56d867",
+    "url": "/li4_umfit/static/js/48.29eb938a.chunk.js"
+  },
+  {
+    "revision": "f95850045d2156ce6494",
+    "url": "/li4_umfit/static/js/49.fad14bed.chunk.js"
+  },
+  {
+    "revision": "f0f1036b944fcd64d341",
+    "url": "/li4_umfit/static/js/5.dcdc0f55.chunk.js"
+  },
+  {
+    "revision": "fe8ff752d3d38fb603d8",
+    "url": "/li4_umfit/static/js/50.f8c75cf8.chunk.js"
+  },
+  {
+    "revision": "afd4405cbc0cc90cd3f4",
+    "url": "/li4_umfit/static/js/51.27dfee36.chunk.js"
+  },
+  {
+    "revision": "b9d437c54b2a771da038",
+    "url": "/li4_umfit/static/js/52.7d6c46e3.chunk.js"
+  },
+  {
+    "revision": "f92ab34b64c04a6f20df",
+    "url": "/li4_umfit/static/js/53.03b68b3d.chunk.js"
+  },
+  {
+    "revision": "48543e68c38c407d063f",
+    "url": "/li4_umfit/static/js/54.204783ec.chunk.js"
+  },
+  {
+    "revision": "06319a3a16d08cc0341a",
+    "url": "/li4_umfit/static/js/55.fb49ed41.chunk.js"
+  },
+  {
+    "revision": "67f241a8ca8f5aff1ad2",
+    "url": "/li4_umfit/static/js/56.4cd63653.chunk.js"
+  },
+  {
+    "revision": "0b803746207a4df35f97",
+    "url": "/li4_umfit/static/js/57.bb8ca01f.chunk.js"
+  },
+  {
+    "revision": "5be1738f841a477449b4",
+    "url": "/li4_umfit/static/js/58.4ab0481f.chunk.js"
+  },
+  {
+    "revision": "aa4ac08f6c13b24d10b9",
+    "url": "/li4_umfit/static/js/59.287cf99d.chunk.js"
+  },
+  {
+    "revision": "15a0e2036b090b58c093",
+    "url": "/li4_umfit/static/js/60.b9602601.chunk.js"
+  },
+  {
+    "revision": "b92e218aaa38dc7aa1c8",
+    "url": "/li4_umfit/static/js/61.fcc5396c.chunk.js"
+  },
+  {
+    "revision": "d7833dff28275dc69c35",
+    "url": "/li4_umfit/static/js/62.a7077119.chunk.js"
+  },
+  {
+    "revision": "3c855d5387e1bf710107",
+    "url": "/li4_umfit/static/js/63.cae7894b.chunk.js"
+  },
+  {
+    "revision": "babf3063ae095018c996",
+    "url": "/li4_umfit/static/js/64.926799ec.chunk.js"
+  },
+  {
+    "revision": "3e0094dfff3d12352237",
+    "url": "/li4_umfit/static/js/65.0476644f.chunk.js"
+  },
+  {
+    "revision": "daa8862fa6d7e65ed2e2",
+    "url": "/li4_umfit/static/js/66.04f3fb21.chunk.js"
+  },
+  {
+    "revision": "4681b7b4d89ef4151d0c",
+    "url": "/li4_umfit/static/js/67.85b71519.chunk.js"
+  },
+  {
+    "revision": "2768ef5725a14c99dc05",
+    "url": "/li4_umfit/static/js/68.e7bfea71.chunk.js"
+  },
+  {
+    "revision": "60499d7867cac358cb53",
+    "url": "/li4_umfit/static/js/69.ddb9e948.chunk.js"
+  },
+  {
+    "revision": "2637f866338a726efae3",
+    "url": "/li4_umfit/static/js/70.8372edd2.chunk.js"
+  },
+  {
+    "revision": "e88b04bb38cf1a7283e4",
+    "url": "/li4_umfit/static/js/71.b60d9868.chunk.js"
+  },
+  {
+    "revision": "29af9cfe392b25e59faa",
+    "url": "/li4_umfit/static/js/72.1082f17c.chunk.js"
+  },
+  {
+    "revision": "b65e4b35e8ae03fbb768",
+    "url": "/li4_umfit/static/js/73.bc5c8cb6.chunk.js"
+  },
+  {
+    "revision": "048c41f764f573e73c05",
+    "url": "/li4_umfit/static/js/74.839f3f5f.chunk.js"
+  },
+  {
+    "revision": "598f324e43422adec765",
+    "url": "/li4_umfit/static/js/75.a2722e93.chunk.js"
+  },
+  {
+    "revision": "d1078f23c71022d2637a",
+    "url": "/li4_umfit/static/js/76.36df8315.chunk.js"
+  },
+  {
+    "revision": "4c6db7c27784370e0423",
+    "url": "/li4_umfit/static/js/77.f038e869.chunk.js"
+  },
+  {
+    "revision": "6875b3f7b1ec7e3260f3",
+    "url": "/li4_umfit/static/js/78.6133c32d.chunk.js"
+  },
+  {
+    "revision": "5413969cd541e44c4728",
+    "url": "/li4_umfit/static/js/79.a5db479f.chunk.js"
+  },
+  {
+    "revision": "43568e2b84e375100a39",
+    "url": "/li4_umfit/static/js/80.5159a220.chunk.js"
+  },
+  {
+    "revision": "8f1f78eef62b70cf8de4",
+    "url": "/li4_umfit/static/js/81.9743ff23.chunk.js"
+  },
+  {
+    "revision": "ed4853bc73d353527e81",
+    "url": "/li4_umfit/static/js/82.502643da.chunk.js"
+  },
+  {
+    "revision": "9ff02f8fb53fd1c9ec47",
+    "url": "/li4_umfit/static/js/83.1d1e1bee.chunk.js"
+  },
+  {
+    "revision": "75f5876738d3e7b7ffb3",
+    "url": "/li4_umfit/static/js/84.e9a2fc28.chunk.js"
+  },
+  {
+    "revision": "27bdd0bb5e1489c1037c",
+    "url": "/li4_umfit/static/js/85.69a41b2b.chunk.js"
+  },
+  {
+    "revision": "25a4a6fcec73c52458a6",
+    "url": "/li4_umfit/static/js/86.ee2054a9.chunk.js"
+  },
+  {
+    "revision": "5a0bfaad8d29a1bf7bc3",
+    "url": "/li4_umfit/static/js/87.2725ae98.chunk.js"
+  },
+  {
+    "revision": "14c3a01f37eb65b0547a",
+    "url": "/li4_umfit/static/js/88.96e94112.chunk.js"
+  },
+  {
+    "revision": "2a829b1698754da5f287",
+    "url": "/li4_umfit/static/js/89.09fdc540.chunk.js"
+  },
+  {
+    "revision": "16c07cfa5dccbb0631f5",
+    "url": "/li4_umfit/static/js/90.f3392336.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/li4_umfit/static/js/90.f3392336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a48705957a364d221c",
+    "url": "/li4_umfit/static/js/91.38c953ac.chunk.js"
+  },
+  {
+    "revision": "68667f4c906b10d2eb97",
+    "url": "/li4_umfit/static/js/92.834b6369.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/li4_umfit/static/js/92.834b6369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f54ee0a95e9a26f2075",
+    "url": "/li4_umfit/static/js/93.87ca629c.chunk.js"
+  },
+  {
+    "revision": "ffb1c36b3a46afe589dd",
+    "url": "/li4_umfit/static/js/94.4b307afc.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/li4_umfit/static/js/94.4b307afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844cc498aef6097b7e8d",
+    "url": "/li4_umfit/static/js/95.78f5a61f.chunk.js"
+  },
+  {
+    "revision": "28b8457af17b6d26f10c",
+    "url": "/li4_umfit/static/js/96.edfd62e5.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/li4_umfit/static/js/96.edfd62e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e596d07d254e84050f3e",
+    "url": "/li4_umfit/static/js/main.e8e60657.chunk.js"
+  },
+  {
+    "revision": "f609adae661333e21e21",
+    "url": "/li4_umfit/static/js/runtime-main.25018ab1.js"
+  },
+  {
+    "revision": "5ba218c2be82e54b4b27",
+    "url": "/li4_umfit/static/js/stencil-polyfills-css-shim.fc666c76.chunk.js"
+  },
+  {
+    "revision": "67ce7b0eaf117b7b300c",
+    "url": "/li4_umfit/static/js/stencil-polyfills-dom.724567be.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/li4_umfit/static/js/stencil-polyfills-dom.724567be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ef275b77cf3482e4b5bbff037aa6f0",
+    "url": "/li4_umfit/static/media/404_icon.a0ef275b.png"
+  },
+  {
+    "revision": "4617639f59361ba0612f652cd79f434a",
+    "url": "/li4_umfit/static/media/Afundos.4617639f.png"
+  },
+  {
+    "revision": "d1b2bdc4260c18baa9ecfb4a139aa0cf",
+    "url": "/li4_umfit/static/media/Agachamento_Frontal.d1b2bdc4.png"
+  },
+  {
+    "revision": "001b899ba85aebfa69244c8164b559f9",
+    "url": "/li4_umfit/static/media/Agachamento_Halter.001b899b.png"
+  },
+  {
+    "revision": "5bbdc3c93ea29367a6b657fad475f975",
+    "url": "/li4_umfit/static/media/Agachamento_Normal_Barra.5bbdc3c9.png"
+  },
+  {
+    "revision": "b3707a6727aa78182f1d1a392fa099fb",
+    "url": "/li4_umfit/static/media/Cadeira_Adutora.b3707a67.png"
+  },
+  {
+    "revision": "c078973df63ab12f98562dddbc3b8a26",
+    "url": "/li4_umfit/static/media/Cadeira_Extensora.c078973d.png"
+  },
+  {
+    "revision": "4dd1ea8bca572db171b0a9d00349de40",
+    "url": "/li4_umfit/static/media/Costas_Máquina.4dd1ea8b.png"
+  },
+  {
+    "revision": "b01b3f661d386476eb8ea3443676ca45",
+    "url": "/li4_umfit/static/media/Costas_Pushada.b01b3f66.png"
+  },
+  {
+    "revision": "439289e4f0ea4c3388ec91da0028c7db",
+    "url": "/li4_umfit/static/media/Elevações_Barra.439289e4.png"
+  },
+  {
+    "revision": "c5e08dd577bbad77fe80fbdd134ef21b",
+    "url": "/li4_umfit/static/media/Pack_Deck.c5e08dd5.png"
+  },
+  {
+    "revision": "072acca7e799134e32a88e9538c3af91",
+    "url": "/li4_umfit/static/media/Peito_Press_Maquina.072acca7.png"
+  },
+  {
+    "revision": "a4e321cfa4a3cde6d5b4db0e619ec407",
+    "url": "/li4_umfit/static/media/Serrote_Sentado.a4e321cf.png"
+  },
+  {
+    "revision": "65cd72e4f9097091549e559ad9a64b42",
+    "url": "/li4_umfit/static/media/Supino_Normal_Halters.65cd72e4.png"
+  },
+  {
+    "revision": "2693a03e10bd7f8d6d9b11a0c42d36cf",
+    "url": "/li4_umfit/static/media/Tricep_Corda.2693a03e.png"
+  },
+  {
+    "revision": "e79735e62c90086d06036ee78f81e34b",
+    "url": "/li4_umfit/static/media/Tricep_Frances_Barra.e79735e6.png"
+  },
+  {
+    "revision": "8724362bbbd470d19b0e5d7a425d0b06",
+    "url": "/li4_umfit/static/media/Tricep_KickBack_Corda.8724362b.png"
+  },
+  {
+    "revision": "d36e4e55895f98bd8e4dd5e19fedb067",
+    "url": "/li4_umfit/static/media/acima_do_peso.d36e4e55.png"
+  },
+  {
+    "revision": "d9e3ae0e5a2d2ad551f3b23a5190fe50",
+    "url": "/li4_umfit/static/media/cycling.d9e3ae0e.png"
+  },
+  {
+    "revision": "84f2b75d20c88f43d8618a15f450fffe",
+    "url": "/li4_umfit/static/media/departamento_informatica.84f2b75d.jpg"
+  },
+  {
+    "revision": "f11c947aac88d9747519c1d88ac60a0d",
+    "url": "/li4_umfit/static/media/exercicio_default.f11c947a.png"
+  },
+  {
+    "revision": "66e7b68e79a24ef1ede3ebabb290c6f8",
+    "url": "/li4_umfit/static/media/gym-background.66e7b68e.jpg"
+  },
+  {
+    "revision": "902bcb9b0587d7488a09abfcbbd1ecc2",
+    "url": "/li4_umfit/static/media/jump.902bcb9b.png"
+  },
+  {
+    "revision": "b30d35c135623ec7ad4910481f7e743c",
+    "url": "/li4_umfit/static/media/kickboxing.b30d35c1.png"
+  },
+  {
+    "revision": "4c81a90f80d204033dee85ffb08ec22d",
+    "url": "/li4_umfit/static/media/logo.4c81a90f.png"
+  },
+  {
+    "revision": "39a65197fa453daa06a0b5db8b81473d",
+    "url": "/li4_umfit/static/media/magro.39a65197.png"
+  },
+  {
+    "revision": "79c8ee93812e16edeb963aaa9ff8dd5a",
+    "url": "/li4_umfit/static/media/natacao.79c8ee93.png"
+  },
+  {
+    "revision": "67916cb4164f652c5add2f2549951ecb",
+    "url": "/li4_umfit/static/media/normal.67916cb4.png"
+  },
+  {
+    "revision": "6ef54cdfd2ed0fe20e1d25dcbe453575",
+    "url": "/li4_umfit/static/media/obesidade_morbida.6ef54cdf.png"
+  },
+  {
+    "revision": "9fb7750dab3f3b222b3ce50363eb68b4",
+    "url": "/li4_umfit/static/media/obeso.9fb7750d.png"
+  },
+  {
+    "revision": "510f9e89d493e8b36ae42ab9176c4c38",
+    "url": "/li4_umfit/static/media/pilates.510f9e89.png"
+  },
+  {
+    "revision": "291dd430dc6169a3cad18c0d884bf290",
+    "url": "/li4_umfit/static/media/salsa.291dd430.png"
+  },
+  {
+    "revision": "0075f1cea899057263b49121527c5e58",
+    "url": "/li4_umfit/static/media/zumba.0075f1ce.png"
+  }
+]);
